7fecd056197d1a9a6427912bfeb9075b1e9b95e1
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2023-12-15 01:13:32 +0530

    change backend host

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const appConfig = {
-    backendUrl: 'http://localhost:4000',
-    //backendUrl: 'https://grocerit-backend.onrender.com'
+    //backendUrl: 'http://localhost:4000',
+    backendUrl: 'https://grocerit-backend.onrender.com'
 }
