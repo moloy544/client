2b82f59cbceef762b8eb32ca9d2a4504b14e88e6
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2023-12-22 01:29:53 +0530

    add top loading bar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,8 +30,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
+
         {children}
+
         <SpeedInsights />
+
       </body>
     </html>
   )
