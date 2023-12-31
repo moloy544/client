51427263015711d547f9243306517e99b930e739
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2023-12-31 18:41:35 +0530

    update app ui

--- src/app/components/CategoryGroupSlider.jsx
+++ src/app/components/CategoryGroupSlider.jsx
@@ -5,14 +5,14 @@ export default function CategoryGroupSlider() {
 
   return (
 
-    <div className="w-auto h-auto bg-white flex flex-row items-center overflow-y-scroll gap-3 mobile:gap-2 whitespace-nowrap pt-4 px-2 border border-t-transparent border-b-gray-300 scroll-smooth">
+    <div className="w-auto h-auto bg-yellow-600 flex flex-row items-center overflow-y-scroll gap-3 mobile:gap-2 whitespace-nowrap pt-4 px-2 border border-yellow-600 scroll-smooth">
 
       {categoryArray.map((category) => (
         <Link 
           key={category.id}
           href={`/listing/category/${category.name.toLowerCase().replace(/[' ']/g, '-')}`}
-          className="bg-red-800 w-auto h-auto py-1 px-3 rounded-[4px]">
-          <span className="w-auto h-auto text-xs font-normal text-white">{category.name}</span>
+          className="bg-gray-800 w-auto h-auto py-1 px-3 rounded-[4px]">
+          <span className="w-auto h-auto text-xs font-normal text-gray-200">{category.name}</span>
         </Link>
       ))}
 
--- src/app/listing/category/[categoryName]/page.jsx
+++ src/app/listing/category/[categoryName]/page.jsx
@@ -3,6 +3,7 @@ import { fetchLoadMoreMovies } from "@/utils";
 import { appConfig } from "@/config/config";
 import NavigateBack from "@/app/components/NavigateBack";
 import LoadMoreMoviesGirdWarper from "@/app/components/LoadMoreMoviesGirdWarper";
+import { categoryArray, moviesGenre } from "@/constant/constsnt";
 
 const transformToCapitalizeQuery = (text) => {
 
@@ -62,7 +63,7 @@ export default async function Page({ params }) {
   const { filterResponse, dataIsEnd } = await fetchLoadMoreMovies({
 
     apiPath: apiUrl,
-    limitPerPage: 30, 
+    limitPerPage: 30,
     page: 1
   });;
 
@@ -81,20 +82,106 @@ export default async function Page({ params }) {
         <Link href="/search" className="text-white mr-20 mobile:mr-3 p-1 text-2xl mobile:text-xl">
           <i className="bi bi-search"></i>
         </Link>
+
       </div>
-      
+
+      <FilterDropDown />
+
       <div className="w-full h-full min-h-[90vh] bg-gray-800 py-3 mobile:py-2">
-       
-          <LoadMoreMoviesGirdWarper
+
+        <LoadMoreMoviesGirdWarper
           apiUrl={apiUrl}
           initialPage={1}
           initialMovies={filterResponse}
           isDataEnd={dataIsEnd} />
 
       </div>
+
     </>
   )
 };
+
+function FilterDropDown(){
+
+  return(
+    <div className="space-y-2 fixed top-[70px] right-4 z-50">
+          <details
+            className="overflow-hidden rounded-sm border border-gray-300 [&_summary::-webkit-details-marker]:hidden shadow-xl"
+          >
+            <summary
+              className="flex cursor-pointer items-center justify-between gap-2 bg-white p-2 text-gray-900 transition"
+            >
+              <span className="text-sm font-medium">Filter</span>
+
+              <span className="transition group-open:-rotate-180">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-4 w-4"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+              </span>
+            </summary>
+
+            <div className="border-t border-gray-200 bg-white">
+
+              <ul className="space-y-1 border-t border-gray-200 p-4 max-h-[400px] overflow-y-auto">
+
+              <li>
+                    <div>
+                      <input
+                        type="radio"
+                        name="genre"
+                        value="All"
+                        id="all-genre"
+                        className="peer hidden"
+                      />
+
+                      <label
+                        htmlFor="all-genre"
+                        className="flex cursor-pointer items-center justify-center rounded-md border border-gray-100 bg-white p-2 text-gray-900 hover:border-gray-200 peer-checked:border-yellow-600 peer-checked:bg-yellow-600 peer-checked:text-white  hover:bg-gray-100"
+                      >
+                        <p className="text-sm font-medium">All</p>
+                      </label>
+                    </div>
+                  </li>
+
+                {moviesGenre.map((genre) => (
+
+                  <li key={genre.id}>
+                    <div className="py-2">
+                      <input
+                        type="radio"
+                        name="genre"
+                        value={genre.name}
+                        id={genre.name}
+                        className="peer hidden"
+                      />
+
+                      <label
+                        htmlFor={genre.name}
+                        className="flex cursor-pointer items-center justify-center rounded-md border border-gray-100 bg-white p-1 text-gray-900 hover:border-gray-200 peer-checked:border-yellow-600 peer-checked:bg-yellow-600 peer-checked:text-white  hover:bg-gray-100"
+                      >
+                        <p className="text-sm font-medium">{genre.name}</p>
+                      </label>
+                    </div>
+                  </li>
+                ))}
+
+              </ul>
+            </div>
+          </details>
+
+
+        </div>
+
+  )
+
+}
 
 
 
