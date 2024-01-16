dbcaa08bcd342c16972f20b5faf5d8a39d13281d
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-01-16 15:26:47 +0530

    fix not-found page error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -9,7 +9,7 @@ export default function NotFound() {
 
                 <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
 
-                <p className="mt-4 text-gray-500">We can't find that page.</p>
+                <p className="mt-4 text-gray-500">{"We can't find that page."}</p>
 
                 <Link
                     href="/"
