7a0dd42cc23d6f51ddeb6aab1b8812de0f9d619e
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-06-18 23:56:13 +0530

    add console for see what happend after deploy

--- src/app/api/embed/route.js
+++ src/app/api/embed/route.js
@@ -16,6 +16,8 @@ export async function GET(req) {
 
         // Extract the referer domain
         const referer = req.headers.get('referer');
+console.log(req.headers);
+console.log(req)
 
         if (!referer) {
             return new NextResponse(creatErrorMessage("Something Went Wrong!"), {
