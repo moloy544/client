acd9590e1e8f0af40707d432e994aa02525e7f0f
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-06-19 01:18:45 +0530

    fix issue

--- src/app/api/embed/route.js
+++ src/app/api/embed/route.js
@@ -1,46 +1,8 @@
 import { NextResponse } from "next/server";
 
-const creatErrorMessage = (message) => {
-    return `<html>
-            <body style="background: black">
-            <div style="color: white;font-family: monospace;text-align: center;top: calc(50%);height: 7%;vertical-align: middle;position: relative;">
-            ${message}
-            </div>
-            </body>
-            </html>`
-};
-
 export async function GET(req) {
     try {
-        const allowedDomains = ['watchyouflix.com']; // List of allowed domains
-
-        // Extract the referer domain
-        const referer = req.headers.get('origin');
-console.log(req);
-console.log(`origin is ${referer}`)
-
-        if (!referer) {
-            return new NextResponse(creatErrorMessage("Something Went Wrong!"), {
-                headers: {
-                    'Content-Type': 'text/html',
-                },
-                status: 403
-            });
-        }
-
-        const refererUrl = new URL(referer);
-        const refererDomain = refererUrl.hostname;
-
-        // Check if the referer domain is allowed
-        if (!allowedDomains.includes(refererDomain)) {
-            return new NextResponse(creatErrorMessage("Forbidden: Domain not allowed"), {
-                headers: {
-                    'Content-Type': 'text/html',
-                },
-                status: 403
-            });
-        }
-
+       
         const requestUrl = new URL(req.url);
         const { searchParams } = requestUrl;
         // get imdb id from url search params
@@ -64,7 +26,13 @@ console.log(`origin is ${referer}`)
         console.log(error);
 
         // if any error occurs then return error message
-        return new NextResponse(creatErrorMessage("Internal server error"), {
+        return new NextResponse(creatErrorMessage(`<html>
+            <body style="background: black">
+            <div style="color: white;font-family: monospace;text-align: center;top: calc(50%);height: 7%;vertical-align: middle;position: relative;">
+          Internal serve error occurred
+            </div>
+            </body>
+            </html>`), {
             headers: {
                 'Content-Type': 'text/html',
             },
