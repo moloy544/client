8d2c8778e8c2d6834e5a9faa01010778adddd373
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-09-12 02:18:27 +0530

    improve hls player floating mode feature based on device

--- src/components/HlsPlayer.jsx
+++ src/components/HlsPlayer.jsx
@@ -19,7 +19,16 @@ function generateSourceURL(originalURL, userIp) {
     const expirationTimestamp = Math.floor(Date.now() / 1000) + 10 * 60 * 60;
     const modifiedURL = originalURL.replace(/:\d+:\d+\.\d+\.\d+\.\d+:/, `:${expirationTimestamp}:${userIp}:`);
     return modifiedURL;
-}
+};
+
+const isMobileDevice = () => {
+    if (navigator.userAgentData) {
+        return navigator.userAgentData.mobile;
+    } else {
+        // Fallback to older method
+        return /Mobi|Android|iPhone|iPad|iPod|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+};        
 
 function VidStackPlayer({ title, source, userIp, playerType = "default" }) {
     const [modifiedSource, setModifiedSource] = useState(source);
@@ -31,6 +40,8 @@ function VidStackPlayer({ title, source, userIp, playerType = "default" }) {
     const mediaPlayerRef = useRef(null);
 
     const isPortrait = useOrientation();
+
+    const isMobile = isMobileDevice();
 
     const isOnline = useOnlineStatus({
         onlineCallback: () => setPlaybackError(null),
@@ -74,50 +85,75 @@ function VidStackPlayer({ title, source, userIp, playerType = "default" }) {
         // player floating and default effect classes
         const floatingClass = 'fixed top-0 left-2/4 sm:left-0 transform -translate-x-2/4 sm:translate-x-1/4 sm:w-[60%] w-[98%] max-w-sm h-auto z-50 rounded-md overflow-hidden opacity-40 transition-all duration-500';
         const staticClass = 'w-full h-full transition-all duration-500';
-        const playerContainerFloatingClass = "w-full h-full flex justify-center items-center transition-all duration-500 shadow-xl shadow-slate-700 p-5";
+        const playerContainerFloatingClass = "w-full h-full flex justify-center items-center transition-all duration-500 shadow-xl shadow-slate-700 p-5 bg-gray-950";
         const playerContainerStaticClass = "w-full h-full flex justify-center items-center transition-all duration-500";
 
         if (!playerContainer || !player) return;
 
         // Handle non-portrait mode
-        if (!isPortrait) {
+        if (!isPortrait && isMobile) {
             if (player.classList.contains("fixed")) {
                 player.classList.remove(...floatingClass.split(' '));
                 player.classList.add(...staticClass.split(' '));
                 playerContainer.classList.remove(...playerContainerFloatingClass.split(' '));
                 playerContainer.classList.add(...playerContainerStaticClass.split(' '));
             };
-
-            // Handle video playback based on intersection
-            if (!entry.isIntersecting) {
-                if (!mediaPlayer.paused) {
-                    mediaPlayer.pause();
-                }
-            } else {
-                if (mediaPlayer.paused) {
-                    mediaPlayer.play();
-                }
-            };
-
+            /**if (mediaPlayer) {
+                // Handle video playback based on intersection
+                if (!entry.isIntersecting) {
+                    if (!mediaPlayer.paused) {
+                        mediaPlayer.pause();
+                    }
+                } else {
+                    if (mediaPlayer.paused) {
+                        mediaPlayer.play();
+                    }
+                };
+            }/** */
             return;
-        }
+        };
+        const playerWidth = player.clientWidth;
+        const playerHeight = player.clientHeight;
 
+        const makeReplacingPlayer = () => {
+            // Create a div element for the replacement player
+            const replacingPlayer = document.createElement('div');
+            replacingPlayer.id = 'replacing-hls-player'; // Assign a unique ID
+            replacingPlayer.className = `w-[${playerWidth}px] h-[${playerHeight}px] bg-gray-950 rounded-md overflow-hidden flex justify-center items-center opacity-40 transition-all duration-500`;
+            // Set inner text
+            replacingPlayer.innerHTML = '<p class="text-white text-sm text-center">Video in floating modee</p>';
 
+            // Append the created element to the playerContainer
+            if (playerContainer) {
+                playerContainer.appendChild(replacingPlayer);
+            }
+
+            return replacingPlayer;
+        };
 
         if (entry.isIntersecting) {
+
             player.classList.remove(...floatingClass.split(' '));
             player.classList.add(...staticClass.split(' '));
             player.removeAttribute('style');
             playerContainer.removeAttribute('style');
+            // Remove the replacing player if it exists
+            const replacingPlayer = document.getElementById('replacing-hls-player');
+            if (replacingPlayer) {
+                playerContainer.removeChild(replacingPlayer);
+            }
             setTimeout(() => {
                 player.classList.remove('opacity-40');
                 playerContainer.classList.remove(...playerContainerFloatingClass.split(' '));
                 playerContainer.classList.add(...playerContainerStaticClass.split(' '));
             }, 300);
+
         } else {
+
             player.classList.remove(...staticClass.split(' '));
             player.classList.add(...floatingClass.split(' '));
             player.removeAttribute('style');
+            makeReplacingPlayer();
             setTimeout(() => {
                 player.classList.remove('opacity-40');
                 playerContainer.classList.remove(...playerContainerStaticClass.split(' '));
