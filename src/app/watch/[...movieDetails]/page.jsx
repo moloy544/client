4d88039a7d0db66585abd09d9d0fa6d34d67e91d
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-09-27 23:47:12 +0530

    chnage ads vast url

--- src/app/watch/[...movieDetails]/page.jsx
+++ src/app/watch/[...movieDetails]/page.jsx
@@ -133,7 +133,7 @@ export default async function Page({ params }) {
         suggestions={suggestions}
         userIp={ip}
       />
-      <Script async={true} src="/static/js/anti-adblock.js" data-cfasync="true" />
+      <Script async={true} src="/static/js/anti-adblock.js" data-cfasync="true" strategy="lazyOnload" />
       <Footer />
     </InspectPreventer>
   )
