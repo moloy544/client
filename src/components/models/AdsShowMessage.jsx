5bb9bfcb5d4925a1ffd1f207717fd7ea991d5cc2
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-10-03 03:51:16 +0530

    improve user management by cookie

--- src/components/models/AdsShowMessage.jsx
+++ src/components/models/AdsShowMessage.jsx
@@ -7,6 +7,7 @@ function AdsShowMessage({ user }) {
     const [showMessage, setShowMessage] = useState(false);
 
     const setUserCookie = async () => {
+        console.log(user)
         if (user) return;
         try {
             await axios.post('/api/user');
