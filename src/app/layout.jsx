ba70f7d269d73d3706b833f03529a0353fd435cc
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-10-08 14:42:23 +0530

    add sociial bar ads again

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -71,7 +71,12 @@ export default function RootLayout({ children }) {
           src=' //filthygracefulspinach.com/de/76/3a/de763a67f50e8441e9ba957065f79f20.js'
           strategy="lazyOnload"
         />
-
+         <Script
+          type='text/javascript'
+          async={true}
+          src='//filthygracefulspinach.com/43/98/8c/43988ce9b59be4684da90ce3bf3e71c5.js'
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   )
