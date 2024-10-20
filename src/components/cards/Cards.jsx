6cfa5ccf36d9c2d83107cdf18c9cf1150d63911f
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-10-20 19:20:30 +0530

    fix actor responsive card style problem

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -52,7 +52,7 @@ const ResponsiveActorCard = ({ data }) => {
 
             <Link href={`/actors/${creatUrlLink(data.name)}/${data.imdbId.replace('nm', '')}`} title={data.name} prefetch={false}>
 
-                <div className="w-auto h-auto rounded-full overflow-hidden border-2 border-yellow-600">
+                <div className="w-auto h-auto aspect-square rounded-full overflow-hidden border-2 border-yellow-600">
 
                     <Image
                         className="w-full object-fill pointer-events-none select-none rounded-full"
@@ -60,7 +60,7 @@ const ResponsiveActorCard = ({ data }) => {
                         height={150}
                         src={data.avatar}
                         placeholder="blur"
-                        blurDataURL={data.thambnail}
+                        blurDataURL={data.avatar}
                         alt={data.name || 'Movies Bazar Actor avatar'}
                     />
 
