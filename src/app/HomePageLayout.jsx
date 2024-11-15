8e99abe4473387b8cb3e9458a51adb962e2022f5
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-11-15 14:58:22 +0530

    fix home page layout actor listing actor card width big in small screen problem

--- src/app/HomePageLayout.jsx
+++ src/app/HomePageLayout.jsx
@@ -131,7 +131,7 @@ function HomePageLayout({ initialLayoutData }) {
                             href={`/actors/${creatUrlLink(actor.name)}/${actor.imdbId?.replace('nm', '')}`}
                             key={actor.imdbId}
                             title={actor.name}
-                            className="w-[calc(100% / 6)] max-w-[150px] min-w-[110px] h-auto px-3 py-2.5 cursor-pointer bg-gray-700 rounded-md flex-shrink-0"
+                            className="w-[calc(100%/6)] aspect-[4/5] max-w-[160px] min-w-[110px] h-auto px-3 py-2.5 cursor-pointer bg-gray-700 rounded-md flex-shrink-0"
                             prefetch={false}
                         >
 
