f3c1e5a77023b0e05363339db71fe74d50fd56bf
Sanjoy Rskhit <sanoyrakshit@gmail.com>
2024-12-25 01:48:09 +0530

    improve brand og image

--- src/constant/assets_links.js
+++ src/constant/assets_links.js
@@ -1,2 +1,2 @@
 // export app base og image URL
-export const BASE_OG_IMAGE_URL = "/static/images/brand_logo_seo.png";
+export const BASE_OG_IMAGE_URL = "https://res.cloudinary.com/ddvep5ney/image/upload/v1735071430/app_brand_og_image.png";
