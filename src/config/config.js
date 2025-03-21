84b0fe6672c2ce42bb6a2022d2ef7f0366a44e0d
sanjoy504 <sanoyrakshit@gmail.com>
2025-03-21 16:29:10 +0530

    add https://moviesbazarapi.vercel.app for backend api

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,4 @@
-import { isIOS } from "@/helper/helper";
-
 export const appConfig = {
     appDomain: process.env.APP_DOMAIN,
-    backendUrl: isIOS() ? "https://moviesbazarapi.vercel.app" :process.env.BACKEND_SERVER_URL,
+    backendUrl: process.env.BACKEND_SERVER_URL,
 }
