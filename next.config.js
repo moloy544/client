522c05bee340870907c46b7355ce8624386c831d
sanjoy504 <sanoyrakshit@gmail.com>
2025-03-23 01:40:16 +0530

    chnage main video server and second video server domain

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
     APP_VERSION: '6.4.0',
     APP_DOMAIN: 'https://www.moviesbazar.net',
     BACKEND_SERVER_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://moviesbazarapi.vercel.app',
-    VIDEO_SERVER_URL: 'https://snowant327arh.com/play/',
-    SECOND_VIDEO_SERVER_URL: 'https://jillw337rpo.com/play/'
+    VIDEO_SERVER_URL: 'https://kinne341wokre.com/play/',
+    SECOND_VIDEO_SERVER_URL: 'https://jole340erun.com/play/'
   },
 
   images: {
