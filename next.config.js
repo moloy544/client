5dc4b2c883916167490ee2a280f0e2efa3af99c7
sanjoy504 <sanoyrakshit@gmail.com>
2025-04-04 03:45:31 +0530

    change current main video server domain simba344doe.com to ftmoh345xme.com

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     APP_VERSION: '6.4.0',
     APP_DOMAIN: 'https://www.moviesbazar.net',
     BACKEND_SERVER_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://moviesbazar-api-v10.vercel.app',
-    VIDEO_SERVER_URL: 'https://simba344doe.com/play/',
+    VIDEO_SERVER_URL: 'https://ftmoh345xme.com/play/',
     SECOND_VIDEO_SERVER_URL: 'https://diego343qop.com/play/'
   },
 
