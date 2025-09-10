b7474d51783a5b74c22b7bc9ca2ad05a4c44447f
sanjoy504 <sanoyrakshit@gmail.com>
2025-09-10 15:58:13 +0530

    Temporary remove Broadcast component

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
     APP_VERSION: '18.4.0',
     DOMAIN: 'https://www.moviesbazar.net',
     API_SERVER_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://moviesbazar-api-v8.vercel.app',
-    VIDEO_SERVER_URL: 'https://jeyna376dip.com/play/', 
-    SECOND_VIDEO_SERVER_URL: 'https://fuhho374key.com/play/'
+    VIDEO_SERVER_URL: 'https://krabo377oun.com/play/', 
+    SECOND_VIDEO_SERVER_URL: 'https://jeyna376dip.com/play/'
   },
 
   images: {
